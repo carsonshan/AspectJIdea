c4b058e4f4e04f227a3e1abb9e404155d79274dc
noprom <noprom@nopromdeMacBook-Pro.local>
2016-03-18 20:42:07 +0800

    update app

--- src/main/java/aspectj/trace/code/app/App.java
+++ src/main/java/aspectj/trace/code/app/App.java
@@ -11,68 +11,74 @@ public class App {
     public void A1() {
         System.out.println("In A1");
         A2();
+        B6();
+        C10();
+        C9();
+        C8();
     }
 
     public void A2() {
         System.out.println("In A2");
         A3();
+        B5();
     }
 
     public void A3() {
         System.out.println("In A3");
         A4();
+        B4();
     }
 
     public void A4() {
         System.out.println("In A4");
         A5();
+        B3();
     }
 
     public void A5() {
         System.out.println("In A5");
         A6();
+        B2();
     }
 
     public void A6() {
         System.out.println("In A6");
         A7();
+        B1();
     }
 
     public void A7() {
         System.out.println("In A7");
         A8();
+        A9();
+        A10();
+        B1();
+        B2();
+        B3();
     }
 
     public void A8() {
         System.out.println("In A8");
-        A9();
     }
 
     public void A9() {
         System.out.println("In A9");
-        A10();
-        B1();
     }
 
     public void A10() {
         System.out.println("In A10");
-        B2();
     }
 
     public void B1() {
         System.out.println("In B1");
-        B2();
     }
 
     public void B2() {
         System.out.println("In B2");
-        B3();
     }
 
     public void B3() {
         System.out.println("In B3");
-        B4();
-        B5();
     }
 
     public void B4() {
@@ -81,28 +87,33 @@ public class App {
 
     public void B5() {
         System.out.println("In B5");
-        B6();
     }
 
     public void B6() {
         System.out.println("In B6");
         B7();
-        B8();
+        B7();
+        C10();
     }
 
     public void B7() {
         System.out.println("In B7");
         B8();
+        B8();
+        B8();
+        C9();
     }
 
     public void B8() {
         System.out.println("In B8");
         B9();
+        C8();
     }
 
     public void B9() {
         System.out.println("In B9");
         B10();
+        C7();
     }
 
     public void B10() {
@@ -112,11 +123,11 @@ public class App {
         C3();
         C4();
         C5();
+        C6();
     }
 
     public void C1() {
         System.out.println("In C1");
-        C2();
     }
 
     public void C2() {
@@ -154,7 +165,9 @@ public class App {
 
     public void C8() {
         System.out.println("In C8");
-        C9();
+        for (int i = 0; i < 10; i++) {
+            C10();
+        }
     }
 
     public void C9() {
