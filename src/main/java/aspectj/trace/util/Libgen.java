0d838d18fa365b21e66921399b24b4d58d65e05a
noprom <tyee.noprom@qq.com>
2016-04-30 10:32:08 +0800

    add libgen download

---
+++ src/main/java/aspectj/trace/util/Libgen.java
@@ -0,0 +1,45 @@
+package aspectj.trace.util;
+
+import org.apache.log4j.Logger;
+
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+
+/**
+ * Libgen
+ *
+ * Author: Noprom <tyee.noprom@qq.com>
+ * Date: 4/30/16 9:42 AM.
+ */
+public class Libgen {
+
+    Logger logger = Logger.getLogger(getClass());
+
+    private void download() {
+        String fileName = "/Volumes/Mac/Libgen/repository_torrent/magnet-links.txt";
+        try {
+            BufferedReader reader = new BufferedReader(new FileReader(fileName));
+            String line;
+            String cmd;
+            while ((line = reader.readLine()) != null) {
+                line = line.split("magnet")[0];
+                line = line.trim();
+                line = "http://libgen.io/repository_torrent/" + line;
+                cmd = "wget -P /Volumes/Mac/Libgen/repository_torrent " + line;
+                ExecUtil.exec(cmd);
+            }
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        Libgen libgen = new Libgen();
+        libgen.download();
+    }
+}
